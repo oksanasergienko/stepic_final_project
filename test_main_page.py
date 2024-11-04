7314701ca7e12ab4be5d7b2e7c3d652b9ab73d2a
Oksana <ok.sergienko@gmail.com>
2024-11-04 10:14:28 +0100

    Add locators.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,16 @@
 from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
+# def test_guest_can_go_to_login_page(browser):
+    # link = "http://selenium1py.pythonanywhere.com/"
+    # page = MainPage(browser, link)      # initialize Page Object, pass the driver instance and url address to the constructor
+    # page.open()                         # open page
+    # page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)      # initialize Page Object, pass the driver instance and url address to the constructor
-    page.open()                         # open page
-    page.go_to_login_page()
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
